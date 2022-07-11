7c46cf144f9a6119d68421481673c07604b42adc
Nikita Lezhnev <niklezhnev44@gmail.com>
2022-07-11 14:25:43 +0300

    collections: add ordered_collections.js

---
+++ collections/iterating_and_iterables/ordered_collections.js
@@ -0,0 +1,12 @@
+// iterables are ordered collections
+// that means you get elements in order
+const abc = ["a", "b", "c"];
+for (const i of abc) {
+    console.log(i)
+}
+for (const i of abc) {
+    console.log(i)
+}
+// we recreate it by returning a brand new iterator
+
+// we are interested in ordered collections only currently
